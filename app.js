ff47afe3bba3c8bd81ae94a4dd0d762f42867db1
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@MacBook-Pro-de-Andres.local>
2019-11-21 11:46:49 +0100

    Finished configuration and now, I'll start to try in postman

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    
-})
+    res.status(200).send('Hello from the server side!');
+});
 
 const port = 3000;
 app.listen(port, () => {

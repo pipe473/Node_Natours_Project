18d5ea4070c1adeb5882a4b2fc17b68092ae5f71
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@MacBook-Pro-de-Andres.local>
2019-11-21 12:23:35 +0100

    Testing the functionality of server with postman, sendind status see results on server

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const app = express();
 
+// Send status from server side
+
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from the server side!');
+    res.status(200)
+    //.send('Hello from the server side!');
+    .json({message: 'Hello from the server side!', app:'Natours' });
 });
 
+// Listening port
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);    

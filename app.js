b8d70a756342bbea288156e6eacc377f027b416a
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@MacBook-Pro-de-Andres.local>
2019-11-21 13:57:15 +0100

    On this point start the final project

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const app = express();
 
 // Send get methods from server, depending status
 
-app.get('/', (req, res) => {
-    res.status(200)
-    //.send('Hello from the server side!');
-    .json({message: 'Hello from the server side!', app:'Natours' });
-});
+// app.get('/', (req, res) => {
+//     res.status(200)
+//     //.send('Hello from the server side!');
+//     .json({message: 'Hello from the server side!', app:'Natours' });
+// });
 
-app.post('/', (req, res) => {
-    res.send('You can post to endpoint...');
-});
+// app.post('/', (req, res) => {
+//     res.send('You can post to endpoint...');
+// });
+
+app.get('/api/v1/tours', (req, res) => {
+    
+})
 
 // Listening port
 const port = 3000;

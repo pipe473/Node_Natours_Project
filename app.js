b20faba292dd967810f7fcb231e19c2d9aeb6741
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-23 20:28:45 +0100

    Testing a post method without middleware (it is how if we'd disconnected the app to server)

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 //middleware
-app.use(express.json());
+//app.use(express.json());
 
 // Send get methods from server, depending status
 

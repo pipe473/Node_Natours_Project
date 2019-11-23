af55c3d78da425889ae84dd891d9f0ba952e35e0
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-23 20:46:17 +0100

    Using a POST to create new dates in the json file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 //middleware
-//app.use(express.json());
+app.use(express.json());
 
 // Send get methods from server, depending status
 
@@ -33,10 +33,20 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-    console.log(req.body);
-    res.send('Done');
-    
-})
+    //console.log(req.body);
+    const newId = tours[tours.length -1].id +1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+});
 
 // Listening port
 const port = 3000;

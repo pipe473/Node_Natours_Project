bcca4e070134176bb1a5838079553f444b95cedc
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-23 21:17:53 +0100

    Adding a solution for when the id is over last number

--- app.js
+++ app.js
@@ -33,33 +33,45 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.get('/api/v1/tours/:id', (req, res) => {
-    console.log(req.params);
+  console.log(req.params);
 
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-    
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour
-      }
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id'
     });
+  }
+
+  const tour = tours.find(el => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
   });
+});
 
 app.post('/api/v1/tours', (req, res) => {
-    //console.log(req.body);
-    const newId = tours[tours.length -1].id +1;
-    const newTour = Object.assign({ id: newId }, req.body);
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
 
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
-    });
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 // Listening port

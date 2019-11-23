c1107087532a423a28865dbb355f8cc72cf093ec
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-23 20:02:30 +0100

    Getting all json information file responsing a 200 to get tours object array

--- app.js
+++ app.js
@@ -16,15 +16,20 @@ const app = express();
 // });
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-    );
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
-
-})
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours
+    }
+  });
+});
 
 // Listening port
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);    
+  console.log(`App running on port ${port}...`);
 });

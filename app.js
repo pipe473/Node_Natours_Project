f9274bd0f5ed829978bbd5d928e76e989bd5bb13
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-23 21:49:05 +0100

    Handling DELETE requests

--- app.js
+++ app.js
@@ -91,6 +91,19 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+   return res.status(404).json({
+     status: 'fail',
+     message: 'Invalid Id'
+   });
+ }
+ res.status(204).json({
+   status: 'success',
+   data: null
+ });
+});
+
 // Listening port
 const port = 3000;
 app.listen(port, () => {

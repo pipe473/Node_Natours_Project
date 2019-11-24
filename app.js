45f535babe21aa87344b8964b4398e955edaed4f
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-24 20:19:43 +0100

    A better file structure finished

--- app.js
+++ app.js
@@ -26,9 +26,6 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// START SERVER
+module.exports = app;
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+

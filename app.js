b593728103753afafd86f6b0e865cf265b09ffd2
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-24 17:13:04 +0100

    Creating our own middleware

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const fs = require('fs');
 const express = require('express');
 
-const app = express();
-
 //middleware
+
+const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware 🖖');
+    next();
+    });
+
+    app.use((req, res, next) => {
+        req.requestTime = new Date().toISOString();
+        next();
+    });
+
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+    console.log(req.requestTime);
+    
     res.status(200).json({
       status: 'success',
+      requestedAt: req.requestTime,
       results: tours.length,
       data: {
         tours
@@ -95,6 +109,8 @@ const deleteTour = (req, res) => {
 // app.get('/api/v1/tours/:id', getTour);
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
+
+
 
 app
 .route('/api/v1/tours')

3f220d4a3d2a42a6355c81d232c4393a99db113a
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-11-30 16:37:50 +0100

    Creating a basic schema

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connections succesful...!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

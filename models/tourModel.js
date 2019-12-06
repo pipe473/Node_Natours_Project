e22bf06ec4298e250c2ad330c0af28dd229c0d64
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-12-06 15:49:52 +0100

    Modeling the Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,9 +4,10 @@ const tourSchema = new mongoose.Schema({
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
-      unique: true
+      unique: true,
+      trim: true 
     },
-    durations: {
+    duration: {
       type: String,
       required: [true, 'A tour must have a name']
     },
@@ -22,10 +23,38 @@ const tourSchema = new mongoose.Schema({
       type: Number,
       default: 4.5
     },
+    ratingAverage: {
+      type: Number,
+      default: 4.5
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0
+    },
     price: {
       type: Number,
       required: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date]
   });
   
   const Tour = mongoose.model('Tour', tourSchema);

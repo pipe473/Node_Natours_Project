69a3d34db9f50aed5835c728481c19956b819029
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-12-06 14:25:07 +0100

    Another way of Creating Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,15 +4,7 @@ const Tour = require('./../models/tourModel');
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price'
-    });
-  }
-  next();
-};
+
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -38,15 +30,27 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
+
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  }catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent...'
+    });
+  }
+};
   
-      res.status(201).json({
-        status: 'success',
-        // data: {
-        //   tour: newTour
-        // }
-      });
-    };
 
 exports.updateTour = (req, res) => {
   res.status(200).json({

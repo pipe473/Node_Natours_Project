4d5dd2e184d5205f7991282d06d5d3cc335d2cbf
Andres Felipe Bedoya Guzman <andresfelipebedoyaguzman@mbp-de-andres.intern>
2019-12-08 17:52:45 +0100

    Aggregation Pipeline: Unwinding and projecting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -13,6 +13,10 @@ router
   .route('/tour-stats')
   .get(tourController.getTourStats);
 
+  router
+  .route('/monthly-plan/:year')
+  .get(tourController.getMonthlyPlan);
+
 router
   .route('/')
   .get(tourController.getAllTours)
